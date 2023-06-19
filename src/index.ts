db800ef7f21e47e0828e07c0d6c389010e5cefe6
Anderson Leonardo <105087327+andersonScastilho@users.noreply.github.com>
2023-06-19 16:11:22 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-import { CrawlerCorinthiansController } from 'controller/crawler-corinthians.controller';
-import { CrawlerPalmeirasController } from 'controller/crawler-palmeiras.controller';
-
 class Init {
   constructor() {
     this._init();
   }
   private _init() {
     // new CrawlerPalmeirasController().init();
-    new CrawlerCorinthiansController().init();
     console.log('inicializado com sucesso');
   }
 }
